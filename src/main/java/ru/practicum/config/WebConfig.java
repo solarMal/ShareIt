59fad5c012ca9eded6e86b7cb500b1e7db1f6d29
Ivan <solarmaltsev@yandex.ru>
2026-04-19 12:01:45 +0300

    создал модели dto и mapper

--- src/main/java/ru/practicum/config/WebConfig.java
+++ src/main/java/ru/practicum/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration // помечает класс как java-config для контекста приложения
 @EnableWebMvc  // призывает импортировать дополнительную конфигурацию для веб-приложений
 @PropertySource("classpath:application.properties")
-@PropertySource(value = "D:\\dev\\practicum\\module4_spring_without_boot\\src\\main\\resources\\application.properties",
+@PropertySource(value = "D:\\dev\\ShareIt\\src\\main\\resources\\application.properties",
         ignoreResourceNotFound = true) // пример для Windows
 public class WebConfig {
 }
---
+++ src/main/java/ru/practicum/user/UserMapper.java
@@ -0,0 +1,7 @@
+package ru.practicum.user;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+}
