87bd9fcce97aa191aeb2fef11b50879b4468effe
Ivan <solarmaltsev@yandex.ru>
2026-04-22 19:10:02 +0300

    добавлены CRUD методы для Item

--- src/main/java/ru/practicum/user/UserController.java
+++ src/main/java/ru/practicum/user/UserController.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
+    List<User> users;
 
 
     @Autowired
